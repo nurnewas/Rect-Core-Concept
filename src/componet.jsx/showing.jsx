84c17606a6dd143aa059f74e4b7e396da65e3c07
nurnewas <nurnewasjr@gmail.com>
2025-07-19 11:30:22 -0600

    trying to understanding Componenet

---
+++ src/componet.jsx/showing.jsx
@@ -0,0 +1,13 @@
+function Developer() {
+  return (
+    <>
+      <div className="developer">
+        <h2> This is a developer</h2>
+        <p>I create amazing applications</p>
+        <i>let's Shine Our Future </i>
+      </div>
+    </>
+  );
+}
+
+// export defout Developer;
